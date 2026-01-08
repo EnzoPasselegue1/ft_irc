e923b236f24d99a1a6a40580f55fe4c2edcb7acd
enpassel <passelegueenzo@gmail.com>
2026-01-08 18:21:54 +0100

    Add : Server.cpp->function run(main boucle), need to to others function to make it work. add stop(stop the server)

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -55,7 +55,6 @@ Server::~Server()
 */
 bool Server::init()
 {
-	// 1. Créer le socket
 	_serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if (_serverSocket == -1)
 	{
@@ -63,8 +62,6 @@ bool Server::init()
 	    return false;
 	}
 
-	// 2. Configurer SO_REUSEADDR pour pouvoir réutiliser le port
-	//    immédiatement après fermeture du serveur
 	int opt = 1;
 	if (setsockopt(_serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
 	{
@@ -73,8 +70,6 @@ bool Server::init()
 	    return false;
 	}
 
-	// 3. Mettre le socket en mode non-bloquant
-	//    IMPORTANT : Requis par le sujet, obligatoire
 	if (fcntl(_serverSocket, F_SETFL, O_NONBLOCK) == -1)
 	{
 	    std::cerr << "Error: fcntl() failed" << std::endl;
@@ -82,14 +77,12 @@ bool Server::init()
 	    return false;
 	}
 
-	// 4. Configurer l'adresse
 	struct sockaddr_in serverAddr;
 	std::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;           // IPv4
 	serverAddr.sin_addr.s_addr = INADDR_ANY;   // Accepte toutes les interfaces
 	serverAddr.sin_port = htons(_port);        // Port en network byte order
 
-	// 5. Lier le socket au port
 	if (bind(_serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1)
 	{
 	    std::cerr << "Error: bind() failed - port " << _port << " may be in use" << std::endl;
@@ -97,7 +90,6 @@ bool Server::init()
 	    return false;
 	}
 
-	// 6. Mettre en écoute
 	if (listen(_serverSocket, MAX_CLIENTS) == -1)
 	{
 	    std::cerr << "Error: listen() failed" << std::endl;
@@ -105,8 +97,75 @@ bool Server::init()
 	    return false;
 	}
 
-	// 7. Ajouter le socket serveur à poll
 	addToPoll(_serverSocket);
 
 	return true;
 }
+
+/* ========================================================================== */
+/*                       BOUCLE PRINCIPALE                                    */
+/* ========================================================================== */
+
+// 1. Appeler poll() avec un timeout (ex: 1000ms)
+// 2. Parcourir tous les fd surveillés
+// 3. Si c'est le socket serveur, accepter nouvelle connexion
+// 4. Si c'est un client
+// 5. Nettoyer les clients marqués pour déconnexion
+void Server::run()
+{
+	_running = true;
+	while (_running)
+	{
+	    
+	    int pollResult = poll(&_pollFds[0], _pollFds.size(), 1000);
+
+	    if (pollResult == -1)
+	    {
+	        if (errno == EINTR)  // Interrompu par un signal
+	            continue;
+	        std::cerr << "Error: poll() failed" << std::endl;
+	        break;
+	    }
+
+	    if (pollResult == 0)
+	        continue;
+
+	    for (size_t i = 0; i < _pollFds.size(); ++i)
+	    {
+	        // Aucun événement sur ce fd
+	        if (_pollFds[i].revents == 0)
+	            continue;
+
+	        if (_pollFds[i].fd == _serverSocket)
+	        {
+	            if (_pollFds[i].revents & POLLIN)
+	                acceptNewClient();
+	        }
+	        else
+	        {
+	            int clientFd = _pollFds[i].fd;
+
+	            // Erreur ou déconnexion
+	            if (_pollFds[i].revents & (POLLERR | POLLHUP | POLLNVAL))
+	            {
+	                disconnectClient(clientFd);
+	                continue;
+	            }
+	            // Données à lire
+	            if (_pollFds[i].revents & POLLIN)
+	                handleClientData(clientFd);
+
+	            // Prêt à écrire (si des données sont en attente)
+	            if (_pollFds[i].revents & POLLOUT)
+	                flushClientBuffer(clientFd);
+	        }
+	    }
+	    cleanupDisconnectedClients();
+	}
+}
+
+void Server::stop()
+{
+    _running = false;
+}
+
